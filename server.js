88784165c41857b4934f5f02ebe0e79adae5a3d9
Hasrat <hsabit111@gmail.com>
2018-04-17 21:57:00 -0400

    The file is moved to a seperate folder

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+// Middlewares 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+// Handlebars Helpers 
+hbs.registerHelper('getFullYear', () => new Date().getFullYear());
+// Helper with argument
+hbs.registerHelper('transToUpperCase', (text) => {
+    return text.toUpperCase();
+});
+
+app.use((req, res, next) => {
+
+    let log = `${new Date().toString()}: ${req.method} ${req.url}`
+
+    fs.appendFile('activity.log', log + '\n', (err) => {
+        if(err) {
+            console.log(err);
+        }
+    })
+
+    next();
+})
+
+// If page is render and next is not called, it will render the page at all time.
+
+// app.use((req, res, next) => {
+//     res.render('maintenance')
+// })
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        title: 'Home | Web Design',
+        message: 'Welcome to Homepage'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Page',
+        message: 'Welcome to About Page'
+    })
+})
+
+
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`);
+})
